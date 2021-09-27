4dfad758d47c5b7cd8f2636becd69dea6bdb1f70
DL <21921009@zju.edu.cn>
2021-09-27 15:33:07 +0800

    Fix epochs to be 200

--- train_tsp100.sh
+++ train_tsp100.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python run.py --graph_size 100 --baseline rollout \
 --run_name 'tsp100' --problem="tsp" --batch_size=256 --epoch_size=640000 \
---kl_loss=0.01 --n_EG=8 --n_paths=5 --val_dataset=data/tsp100.pkl
+--kl_loss=0.01 --n_EG=8 --n_paths=5 --val_dataset=data/tsp100.pkl --n_epochs=200
--- train_tsp50.sh
+++ train_tsp50.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python run.py --graph_size 50 --baseline rollout \
 --run_name 'tsp50' --problem="tsp" --batch_size=256 --epoch_size=640000 \
---kl_loss=0.01 --n_EG=4 --n_paths=5 --val_dataset=data/tsp50.pkl
+--kl_loss=0.01 --n_EG=4 --n_paths=5 --val_dataset=data/tsp50.pkl --n_epochs=200
